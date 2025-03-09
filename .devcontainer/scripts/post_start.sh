e470c0a533f1affd3552beebf2dfaaf80e8cf30f
Tomoki Ota <tomo.soccer1227@gmail.com>
2025-03-09 20:08:19 +0900

    Initial commit

---
+++ .devcontainer/scripts/post_start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+  echo "Error: No directory provided."
+  exit 1
+fi
+
+#  ~/.gitconfigをローカルからコピーする
+echo "start to copy local ~/.gitconfig"
+git config --global --add safe.directory $1
+
+# ~/.gitconfigの設定追加
+git config pull.rebase false
+git config --global commit.template .commit_template
+echo "Success git config."
+
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir poetry
+
+poetry config virtualenvs.create false
+
+poetry install --no-root
